08b0208959767ee4dde2dd63b766b69102920246
Matthew Stachyra <matthew.stachyra@tufts.edu>
2022-09-19 14:58:46 -0400

    latest

--- init.el
+++ init.el
@@ -16,7 +16,7 @@
  '(custom-enabled-themes '(adwaita))
  '(custom-safe-themes
    '("6ca5f925de5c119694dbe47e2bc95f8bad16b46d154b3e2e0ae246fec4100ec5" "eb122e1df607ee9364c2dfb118ae4715a49f1a9e070b9d2eb033f1cefd50a908" "70cfdd2e7beaf492d84dfd5f1955ca358afb0a279df6bd03240c2ce74a578e9e" default))
- '(fancy-splash-image "~/Desktop/other/default.xpm")
+ '(fancy-splash-image nil)
  '(inhibit-startup-screen nil)
  '(menu-bar-mode nil)
  '(package-selected-packages
